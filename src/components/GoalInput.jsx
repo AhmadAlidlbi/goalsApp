a5bc7ff507df1a27e4041d48511725d45951fb3b
AhmadAlidlbi <200513470@ogr.altinbas.edu.tr>
2024-02-18 15:08:24 +0300

    goalinput

---
+++ src/components/GoalInput.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import {
+  View,
+  TextInput,
+  Button,
+  StyleSheet,
+  Modal,
+  Image,
+} from "react-native";
+
+function GoalInput() {}
+
+export default GoalInput;
+
+const styles = StyleSheet.create({});
